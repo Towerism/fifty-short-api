0c2e29cf5e9ff934d276e2a4302de732f5b8b520
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2021-11-09 22:11:31 -0600

    urls: use hooks instead of overriding class methods

--- src/services/urls/urls.class.js
+++ src/services/urls/urls.class.js
@@ -1,5 +1,4 @@
 const { Service } = require('feathers-objection')
-const { shorturl } = require('@zodash/shorturl')
 
 exports.Urls = class Urls extends Service {
   constructor (options) {
@@ -9,17 +8,5 @@ exports.Urls = class Urls extends Service {
       ...otherOptions,
       model: Model
     })
-  }
-
-  async create ({ url }) {
-    const shortened = shorturl(url)
-    // check if shortened url exists in the database
-    const existing = await this.Model.query().where('shortened', shortened).first()
-    if (existing) {
-      return existing
-    }
-    // store the url in the database
-    const newUrl = await this.Model.query().insert({ url, shortened })
-    return newUrl
   }
 }
--- src/services/urls/urls.hooks.js
+++ src/services/urls/urls.hooks.js
@@ -1,10 +1,20 @@
+const { shorturl } = require('@zodash/shorturl')
 
 module.exports = {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [],
+    create: [async context => {
+      const shortened = shorturl(context.data.url)
+      const existing = await context.service.get(shortened)
+      if (existing) {
+        context.result = existing
+        return context
+      }
+      context.data.shortened = shortened
+      return context
+    }],
     update: [],
     patch: [],
     remove: []
